a20e9f330b8d7d4d40e9685bc79c4cb6c430b9a7
Blake-Lawton <blakelawton777@gmail.com>
2023-11-18 17:21:12 -0800

    comments

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -31,6 +31,9 @@ router.delete('/:id', async (req, res) => {
 
 //This will get our collection of posts so we can preform functions on it.
 async function LoadPostsCollection() {
+
+    // change your username and password here when testing locally.. or you can just use my user it doesnt matter to me. 
+    //You'll just have to make one in mongodb if you want your own
     const client = await mongodb.MongoClient.connect('mongodb+srv://Blake_Lawton:santana77@cluster0.kdlwelg.mongodb.net/?retryWrites=true&w=majority');
    
     return client.db('vue_express').collection('posts');
